7612019ffb6ce66f339a80cecb942ee27b15605a
sophiebl <sboulaao@student.42.fr>
2019-04-29 17:14:33 +0200

    PHP Basic functions

---
+++ Day01/ex12/search_it!.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?PHP
+
+
+function ft_split($string) {
+	$string = preg_replace( "/\:/", " ", $string );
+	return(explode( " ", $string));
+}
+
+array_shift($argv);
+$searched = array_shift($argv);
+$new = array();
+foreach ($argv as $key => $value) {
+	$new = ft_split($value);
+	if ($searched == $new[0]) {
+		print($new[1]."\n");
+	}
+}
+
+
+?>
