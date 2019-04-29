a755a9ed7026fc3e9681ab3da35a676e31b58c50
sophiebl <sboulaao@student.42.fr>
2019-04-29 17:13:08 +0200

    PHP Basic functions

---
+++ Day01/ex07/rostring.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?PHP
+
+if ($argc > 1) {
+	$argv[1] = trim($argv[1]);
+	$argv[1] = preg_replace("/\s+/", " ", $argv[1]);
+	$arg = explode(" ", $argv[1]);
+	$pop = array_shift($arg);
+	array_push($arg, $pop);
+	$str = implode(" ", $arg);
+	print($str."\n");
+}
+
+?>
