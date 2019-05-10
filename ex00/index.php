8a80bb0f3a75794c209e7ce4fbd95022a308a44c
sophiebl <sboulaao@student.42.fr>
2019-05-10 15:56:06 +0200

    PHP

---
+++ ex00/index.php
@@ -0,0 +1,22 @@
+<?php 
+	session_start();
+	if (isset($_GET['submit']) && $_GET['submit'] != "") { 
+		if ($_GET['submit'] === "OK") {
+			$_SESSION["login"] = $_GET["login"];
+			$_SESSION["passwd"] = $_GET["passwd"];
+		} else {
+			exit("ERROR\n");
+		}
+	} 
+?>
+
+<html>
+<body>
+	<form action="." method="GET">
+		Identifiant: <input type="text" name="login" value="<?php if (isset($_SESSION["login"])) { echo $_SESSION["login"]; } ?>">
+		<br/>
+		Mot de passe: <input type="password" name="passwd" value="<?php if (isset($_SESSION["passwd"])) { echo $_SESSION["passwd"]; } ?>">
+		<input type="submit" name="submit" value="OK">
+	</form>
+</body>
+</html>
