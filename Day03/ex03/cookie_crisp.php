b4539de408d5cbace3eca6542045b54f680260f2
sophiebl <sboulaao@student.42.fr>
2019-05-21 15:08:14 +0200

    PHP & Cookies

---
+++ Day03/ex03/cookie_crisp.php
@@ -0,0 +1,8 @@
+<?PHP
+	if ($_GET["action"] == 'set')
+		setcookie($_GET["name"], $_GET["value"], time() + 125600);
+	else if ($_GET["action"] == 'get' && $_COOKIE[$_GET["name"]] != "")
+		echo $_COOKIE[$_GET["name"]]."\n";
+	else if ($_GET["action"] == 'del')
+		setcookie($_GET["name"], "", time() - 3600);
+?>
